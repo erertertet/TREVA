3a0aac26074b3a750119eac99e04e893b42c6bf5
Erertertet <erertertet@gmail.com>
2024-03-02 19:30:08 +0000

    add: test utils

---
+++ backend/test/utils_test.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory of the current script to sys.path
+current_dir = Path(__file__).parent
+parent_dir = current_dir.parent
+model_dir = parent_dir / 'model'
+sys.path.append(str(model_dir))
+
+# Now you can import the test module
+from utils import *
+from sliding_window import *
+
+def test_token_length(m, n):
+    counter = TokenCounter("cl100k_base")
+    a = SrtFile("./backend/model/test.srt")
+    windows = a.generate_slices(counter, m, n)
+    print(windows)
+    for window in windows:
+        lst = a.get_slice(*window)
+        lst = [item[2] for item in lst]
+        assert counter.count_tokens(lst) <= m
+
+    for i in range(len(windows) - 1):
+        l, r = windows[i + 1][0], windows[i][1]
+        lst = a.get_slice(l, r)
+        lst = [item[2] for item in lst]
+        assert counter.count_tokens(lst) >= n
+
+test_token_length(100, 10)
