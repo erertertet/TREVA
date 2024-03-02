ea1335eeb0f3d995c98fbfd1f7ed012b44d938b3
Chivier Humber <chivier.humber@outlook.com>
2024-03-02 19:28:43 +0000

    update: update sliding window test

--- backend/model/sliding_window.py
+++ backend/model/sliding_window.py
@@ -1,7 +1,11 @@
 from utils import *
 
 def generate_punctuated_info(srt_file: SrtFile, sliding_window: list[(int, int)]):
-    pass
+    print("Generating punctuated info")
+    # TODO: update AI model
+    assistant = AICaller()
 
 def merge_sliding_windows(srt_file: SrtFile):
     pass
+
+# generate_punctuated_info(SrtFile("./model/test.srt"), [(0, 1), (1, 2)])
--- backend/test/sliding_window_test.py
+++ backend/test/sliding_window_test.py
@@ -1,3 +1,10 @@
-import model.utils as utils
-import model.sliding_window as sliding_window
+import sys
+import os
+# Get the parent directory path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add the parent directory to the Python path
+sys.path.append(parent_dir)
+
+from model import utils
+from model import sliding_window
