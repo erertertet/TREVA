92aa58fe033bd47e2e121268351a06406f906c66
Chivier Humber <chivier.humber@outlook.com>
2024-03-02 18:08:30 +0000

    add: AI caller

--- backend/model/util.py
+++ backend/model/util.py
@@ -1,4 +1,23 @@
 import tiktoken
+from openai import OpenAI
+import os
+import dotenv
+
+# Global settings for openAI prompt
+GLOBAL_DEFAULT_SETTINGS = {
+    "temperature": 0.7,
+    "max_input_tokens": 8192,
+    "max_output_tokens": 4096,
+    "top_p": 1.0,
+    "top_k": 40,
+    "presence_penalty": 0.0,
+    "frequency_penalty": 0.0,
+    "best_of": 1,
+    "n": 1,
+    "stop_sequence": None,
+    "logprobs": None,
+    "stream": None,
+}
 
 
 class TokenCounter:
@@ -61,18 +80,134 @@ class SrtFile:
 
 
 class AICaller:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, provider="OPENAI", model="gpt-3.5-turbo") -> None:
+        if provider == "OPENAI":
+            self.provider = provider
+            self.api_key = os.environ.get("OPENAI_API_KEY")
+            self.model = model
+            self.settings = GLOBAL_DEFAULT_SETTINGS
+        else:
+            raise ValueError("Invalid provider")
 
-    def send():
-        pass
+    async def send(self, system_role, user_role):
+        # Send general message to the AI assistant
+        client = OpenAI()
+
+        completion = await client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": system_role},
+                {"role": "user", "content": user_role},
+            ],
+        )
+
+        return await completion.choices[0].message.content
+
+    def common_sentence_connect(self, text):
+        client = OpenAI()
+
+        completion = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a literature master. You will be given some sentencies, but some information \
+                            is missing. We do not have some punctuations and we do not seperate the paragraphs \
+                            correctly. Please help us to fix the text.",
+                },
+                {
+                    "role": "user",
+                    "content": "Please fix the text. This text lose some punctuations. Please add the correct \
+                            punctuations and seperate the paragraphs by the meaning of context correctly. And only \
+                            give me the final content please. Here is the text: "
+                    + text,
+                },
+            ],
+        )
+
+        return completion.choices[0].message.content
 
 
 if __name__ == "__main__":
+    # TokenCounter test
     with open("./backend/model/test.srt", "r") as file:
         counter = TokenCounter()
         text_list = counter.reader(file)
         counter.create_batches(text_list, 100, 10)
+        
+    # AICaller test
+    text_text = """
+    Professor Paul Bloom:
+What we've been talking about
+
+so far in the course are human
+universals, what everybody
+
+shares.
+So, we've been talking about
+
+language, about rationality,
+about perception,
+
+about the emotions,
+about universals of
+
+development, and we've been
+talking about what people share.
+
+But honestly,
+what a lot of us are very
+
+interested in is why we're
+different and the nature of
+
+these differences and the
+explanation for them.
+
+And that's what we'll turn to
+today.
+
+So first, we'll discuss how are
+people different,
+
+different theories about what
+makes you different in a
+
+psychological way from the
+person sitting next to you,
+
+and then we'll review different
+theories about why people are
+
+different.
+And this is the class which is
+
+going to bother the most people.
+It's not dualism.
+
+It's not evolution.
+It's this because the
+
+scientific findings on human
+psychological differences are,
+
+to many of us,
+shocking and unbelievable.
+
+And I will just try to persuade
+you to take them seriously.
+
+Okay.
+So, how are people different?
+
+Well, there's all sorts of ways.
+Your sexual identity--It is at
+"""
+    assistant = AICaller()
+    result = assistant.common_sentence_connect(
+        text_text
+    )
+    print (result)
 
 # Example usage:
 # sentences = ["Your sentences go here.", "Another one here."]
