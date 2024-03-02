2fbe08d90c1b920c7a1da52b1d6bf97a4fc2bc78
Chivier Humber <chivier.humber@outlook.com>
2024-03-02 18:47:44 +0000

    add: basic frame for sliding window

---
+++ backend/model/sliding_window.py
@@ -0,0 +1,7 @@
+from util import *
+
+def generate_punctuated_info(srt_file: SrtFile, sliding_window: list[(int, int)]):
+    pass
+
+def merge_sliding_windows(srt_file: SrtFile):
+    pass
---
+++ backend/test/sliding_window_test.py
@@ -0,0 +1,3 @@
+import model.util as util
+import model.sliding_window as sliding_window
+
